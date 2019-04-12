f42ccbc35e0b1af55e9f1b4102d4f5c55ca89617
Azzy M <azzymordor@wireless-10-147-74-156.public.utexas.edu>
2019-04-12 16:16:26 -0500

    used FLIP animation, basic homepage layout done

--- code/js/menuScript.js
+++ code/js/menuScript.js
@@ -1,3 +1,74 @@
+var isToggled = false;
+/*FLIP version of sidebar toggle animation*/
+function toggleSideNav() {
+  page = document.querySelector(".page");
+  sidebar = document.querySelector(".sidebar");
+
+  const pagefirst = page.getBoundingClientRect();
+  const sidebarfirst = sidebar.getBoundingClientRect();
+
+  if (isToggled) {
+    closeNav();
+    isToggled = false;
+  } else {
+    openNav();
+    isToggled = true;
+  }
+  const pagelast = page.getBoundingClientRect();
+  const pagedeltaX = pagefirst.left - pagelast.left;
+  const pagedeltaY = pagefirst.top - pagelast.top;
+  const pagedeltaW = pagefirst.width / pagelast.width;
+  const pagedeltaH = pagefirst.height / pagelast.height;
+
+  const sidebarlast = sidebar.getBoundingClientRect();
+  const sidebardeltaX = sidebarfirst.left - sidebarlast.left;
+  const sidebardeltaY = sidebarfirst.top - sidebarlast.top;
+  const sidebardeltaW = sidebarfirst.width / sidebarlast.width;
+  const sidebardeltaH = sidebarfirst.height / sidebarlast.height;
+
+  page.animate(
+    [
+      {
+        transformOrigin: "top left",
+        transform: `
+      translate(${pagedeltaX}px, ${pagedeltaY}px)
+      scale(${pagedeltaW}, ${pagedeltaH})
+    `
+      },
+      {
+        transformOrigin: "top left",
+        transform: "none"
+      }
+    ],
+    {
+      duration: 500,
+      easing: "ease-in-out",
+      fill: "both"
+    }
+  );
+
+  sidebar.animate(
+    [
+      {
+        transformOrigin: "top left",
+        transform: `
+      translate(${sidebardeltaX}px, ${sidebardeltaY}px)
+      scale(${sidebardeltaW}, ${sidebardeltaH})
+    `
+      },
+      {
+        transformOrigin: "top left",
+        transform: "none"
+      }
+    ],
+    {
+      duration: 500,
+      easing: "ease-in-out",
+      fill: "both"
+    }
+  );
+}
+
 /* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
 function openNav() {
   document.getElementById("sideMenu").style.width = "15%";
@@ -7,14 +78,14 @@ function openNav() {
 
 /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
 function closeNav() {
-  document.getElementById("sideMenu").style.width = "0";
-  document.getElementById("page").style.marginLeft = "0";
+  document.getElementById("sideMenu").style.width = "0%";
+  document.getElementById("page").style.marginLeft = "0%";
   document.getElementById("page").style.width = "100%";
 }
 
 function createMenu() {
   document.getElementById("sideMenu").innerHTML =
-    '<a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a> \n' +
+    '<a href="javascript:void(0)" class="closebtn" onclick="toggleSideNav()">&times;</a> \n' +
     '<a href="#">Home</a> \n' +
     '<a href="#">Kana</a> \n' +
     '<a href="#">Kanji</a> \n' +
